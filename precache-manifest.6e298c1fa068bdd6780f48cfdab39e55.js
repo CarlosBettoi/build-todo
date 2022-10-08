457ee8b4e88e6ecabe51fb819bcb20e9921d3ca0
Carlos Bettoi <carlosbettoi@yahoo.com>
2022-10-08 11:36:05 -0300

    primeira versao

---
+++ precache-manifest.6e298c1fa068bdd6780f48cfdab39e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9999a1ba2058e4205fce",
+    "url": "./static/css/main.76cfa6d5.chunk.css"
+  },
+  {
+    "revision": "9999a1ba2058e4205fce",
+    "url": "./static/js/main.9999a1ba.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "./static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "1cad2ad444499c6709c4",
+    "url": "./static/js/2.1cad2ad4.chunk.js"
+  },
+  {
+    "revision": "0a0172db13975cdd633a",
+    "url": "./static/js/runtime~main.0a0172db.js"
+  },
+  {
+    "revision": "bb2cf78d4aa052253f475178f91d880d",
+    "url": "./index.html"
+  }
+];
